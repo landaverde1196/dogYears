a05f16d5a436fa118e96cd3e58aef8f00ab68061
landaverde1196 <alejndro1196@gmail.com>
2020-02-18 11:46:19 -0600

    first commit

---
+++ dogYears.js
@@ -0,0 +1,10 @@
+const myAge = 23; // variable equal my age
+let earlyYears = 2; //2 first years
+earlyYears *= 10.5;
+let laterYears = myAge- 2; //after the first 2 years
+laterYears *= 4; //each dog years is 4 human yrs
+console.log(earlyYears);
+let myAgeInDogYears = earlyYears + laterYears; //my age in dog
+let myName = 'Alejandro'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} in dog years.`);
+
